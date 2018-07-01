39ec399c80e0c9d6a1e60854e7d5b561d6fa1279
Jackson Toomey <jacksontoomey@gmail.com>
2018-07-01 13:59:24 -0400

    why buttons?

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import netlifyIdentity from 'netlify-identity-widget'
 
-var buttons = document.createElement('div')
-
 netlifyIdentity.init()
 
 ReactDOM.render(<App />, document.getElementById('root'));
