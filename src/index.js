4c19bd2a4054b3e38ee71532a3023e502cd20649
Jackson Toomey <jacksontoomey@gmail.com>
2018-07-01 14:10:45 -0400

    remove service worker

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import netlifyIdentity from 'netlify-identity-widget'
 
 netlifyIdentity.init()
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
