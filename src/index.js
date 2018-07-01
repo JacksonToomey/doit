323ea801fffeae9005f5c0d8599db114503dbb32
Jackson Toomey <jacksontoomey@gmail.com>
2018-07-01 13:52:47 -0400

    more ident

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import netlifyIdentity from 'netlify-identity-widget'
+
+netlifyIdentity.init();
+
+netlifyIdentity.open();
+
+// Get the current user:
+const user = netlifyIdentity.currentUser();
+
+// Bind to events
+netlifyIdentity.on("init", user => console.log(user));
+netlifyIdentity.on("login", user => console.log(user));
+netlifyIdentity.on("logout", () => console.log("Logged out"));
+netlifyIdentity.on("error", err => console.error("Logged out"));
+netlifyIdentity.on("open", () => console.log("Widget opened"));
+netlifyIdentity.on("close", () => console.log("Widget closed"));
+
+// Close the modal
+// netlifyIdentity.close();
+
+// Log out the user
+netlifyIdentity.logout();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
