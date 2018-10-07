0cf3d3079ed99bcfb79b0da95195084ae6a77a40
Jackson Toomey <jacksontoomey@gmail.com>
2018-10-07 18:00:25 -0400

    restart

--- src/App/index.js
+++
@@ -1,4 +0,0 @@
-import { connect } from 'react-redux';
-import App from './Component';
-
-export default connect()(App);
--- src/auth/types.js
+++
@@ -1,5 +0,0 @@
-const PREFIX = '!AUTH__/';
-
-export default {
-  SET_USER: `${PREFIX}SET_USER`,
-};
