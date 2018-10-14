99d5616febaf75bf4d7ef0ea4bf55579166cb519
Jackson Toomey <jacksontoomey@gmail.com>
2018-10-14 09:46:49 -0400

    auth flow

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { initializeCurrentLocation } from 'redux-little-router';
+import { CircularProgress } from 'react-md';
 import netlifyIdentity from 'netlify-identity-widget';
 import App from 'App/Container';
 import actions from 'user/actions';
 import createStore from 'store';
+import api from 'api';
 
 import 'react-md/dist/react-md.blue_grey-indigo.min.css';
 
 import './main.scss';
 
 netlifyIdentity.init();
+
+render(
+  <div><CircularProgress id="main-app-loader" /></div>,
+  document.getElementById('app'),
+);
 
 const load = (u) => {
   const store = createStore();
@@ -33,10 +40,15 @@ const load = (u) => {
 const user = netlifyIdentity.currentUser();
 if (user) {
   netlifyIdentity.close();
-  load(user);
+  const { token } = user;
+  api.post('/login', { token: token.access_token }).then(() => {
+    load(user);
+  });
 } else {
   netlifyIdentity.on('login', async (u) => {
     netlifyIdentity.close();
+    const { token } = u;
+    await api.post('/login', { token: token.access_token });
     load(u);
   });
   netlifyIdentity.open();
